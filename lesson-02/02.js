2ec28c54350a08d8463d428917b5245f5a6b29e9
PavelKrevchik <pashakrevchik@yandex.by>
2026-01-13 09:44:39 +0300

    save-02-03

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,23 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.ceil(Math.random() * 6);
+let dice2 = Math.ceil(Math.random() * 6);
+let isWinningDouble = false
 
-/*
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
-} else {
-  console.log('Не выигрышный дубль.')
-}
-*/
+
+
+  
+  if ((dice1 > 3) && (dice2 >3) && (dice1 === dice2)) {
+    isWinningDouble =true
+    console.log('Выигрышный дубль!')
+  } else {
+    console.log('Не выигрышный дубль.')
+  }
+
+ 
+
+

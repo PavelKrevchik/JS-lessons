5a1541cbc60e89f3c70034b415647754a2c5be6e
PavelKrevchik <pashakrevchik@yandex.by>
2026-01-14 17:02:48 +0300

    save-02-04

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,27 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+// const score = 70 не соглашусь с const так как число от 0 до 100 может быть любым // тестовое значение, можно изменять
+
+let score = Math.floor(Math.random() * 100)
 let grade
 // your code
+console.log(score);
+
+if (score >= 0 && score <=49) {
+   grade = score 
+   console.log(grade, "F");
+} else if (score >= 50 && score <=69) {
+   grade = score 
+   console.log(grade, "D");
+} else if (score >= 70 && score <=79) {
+   grade = score 
+   console.log(grade, "C");
+} else if (score >= 80 && score <=89) {
+   grade = score 
+   console.log(grade, "B");
+} else {
+   grade = score 
+   console.log(grade, "A");
+}
+
